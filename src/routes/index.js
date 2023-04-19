7352663fffaa108667c727ad80d94a6743d335e3
mohsen <mohsen.bahrami7690@gmail.com>
2023-04-19 10:01:18 +0330

    add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 // require routes
-const anyRouter = require('./any');
+const loginRouter = require('./loginRouter');
+const adminRouter = require('./adminRouter');
+const userRouter = require('./userRouter');
+const notFoundRouter = require('./notFoundRouter');
 // require middlewares
 const error = require('../middlewares/error');
 
 // call routers
-router.use("/any", anyRouter);
+//  >> /api/login
+router.use("/api/login", loginRouter);
+//  >> /api/admin/users
+router.use("/api/admin", adminRouter);
+//  >> /api/user
+//  >> /api/user/task?select-task=taskID
+//  >> /api/user/add-task-partners?select-task=taskID
+router.use("/api/user", userRouter);
+//  >> /not-found
+router.use("/not-found", notFoundRouter);
 
 // handle errors
 router.use(error);
