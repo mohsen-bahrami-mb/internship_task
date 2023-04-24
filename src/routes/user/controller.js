36250cedfb84b3899caa4c65837ff72c1906770b
mohsen <mohsen.bahrami7690@gmail.com>
2023-04-24 19:46:10 +0330

    add: put & delete task photo

--- src/routes/user/controller.js
+++ src/routes/user/controller.js
@@ -1,7 +1,48 @@
+const fs = require('fs');
 const Controller = require('../controller');
 const { taskPriorityEnum } = require('../../models/task')
 
 module.exports = new (class extends Controller {
+
+    async checkUserToUpload(req, res, next) {
+        // get task id of query >> ?select-task=
+        const taskId = req.query["select-task"];
+        let task;
+        try { task = await this.Task.findById(taskId); } catch { }
+        if (!task) return this.response({ res, sCode: 404, message: 'cannot find task id in query(select-task=)' });
+        // check user is partner in this task
+        const isUserTask = req.user.tasks.find(t => t._id == taskId);
+        if (!isUserTask) return this.response({ res, sCode: 401, message: "access denied - your not partner in this task" });
+        req.targetTask = task;
+        next();
+    }
+
+    async setFile(req, res) {
+        // check send photo
+        if (!req.file) return this.response({ res, message: "No files were uploaded", sCode: 400 });
+        // set & save photo. set url without domin(ip)
+        req.targetTask.images_urls.push(("public/tasks_photos/" + req.file.filename));
+        req.targetTask.save();
+        // send response
+        this.response({
+            res, message: "uploded file, successfully", sCode: 200,
+            data: { filename: req.file.filename }
+        });
+    }
+
+    async deleteFile(req, res) {
+        // check img_url is currect
+        const fileURL = req.body.img_url;
+        const isExistURL = req.targetTask.images_urls.find(e => e.includes(fileURL));
+        if (!isExistURL) return this.response({ res, sCode: 404, message: `cannot find this file ${fileURL}` });
+        // first remove file.then clear url without domin(ip)
+        fs.unlink(fileURL, (err) => { if (err) winston.error(err.message, err) });
+        const newImagesURLs = req.targetTask.images_urls.filter(e => !e.includes(fileURL));
+        req.targetTask.images_urls = newImagesURLs;
+        req.targetTask.save();
+        // send response
+        this.response({ res, message: "delete file, successfully", sCode: 200, });
+    }
 
     async getAllTasks(req, res) {
         const allUserTasksId = req.user.tasks;
@@ -81,6 +122,9 @@ module.exports = new (class extends Controller {
         try {
             // delete task of database
             task = await this.Task.findByIdAndRemove(taskId);
+            // delete file and images
+            task.images_urls.forEach(e =>
+                fs.unlink(e, (err) => { if (err) winston.error(err.message, err) }));
             // delete task of "user.tasks" on database
             let userOldTasks = [...req.user.tasks];
             let userNewTasks = [];
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -2,6 +2,21 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 const validator = require('./validator');
+// require middlewares
+const { upload } = require('../../middlewares/fileUpload');
+const uploadFile = upload("public/tasks_photos/", 1048576, [
+    "image/jpg",
+    "image/jpeg",
+    "image/png",
+    "image/tiff",
+    "audio/mp3",
+    "video/mp4",
+    "application/vnd.ms-excel",
+    "application/vnd.ms-powerpoint",
+    "application/msword",
+    "application/pdf",
+    "application/zip"
+]);
 
 router.get("/all-tasks", controller.getAllTasks);
 router.get("/task", controller.getTask);
@@ -14,6 +29,15 @@ router.put("/task",
     validator.taskValidator(),
     controller.validate,
     controller.editTask
+);
+router.put("/task/upload-task-photo",
+    controller.checkUserToUpload,
+    uploadFile.single('task_photo'),
+    controller.setFile
+);
+router.delete("/task/delete-task-photo",
+    controller.checkUserToUpload,
+    controller.deleteFile
 );
 router.delete("/task", controller.removeTask);
 
